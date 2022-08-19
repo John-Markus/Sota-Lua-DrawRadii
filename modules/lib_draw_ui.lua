4664add62f6ef91546f2a495257bf1075f61b2b6
Minoru TODA <todam@netjapan.co.jp>
2022-08-19 11:07:40 +0900

    Move more code to modules

--- modules/lib_draw_ui.lua
+++ modules/lib_draw_ui.lua
@@ -5,7 +5,7 @@ module.textures = { }
 module.ui_objects = { }
 module.current_ui_object = false;
 
-module.LoadTexturePixel = function(color, path) {
+module.LoadTexturePixel = function(color, path)
 	-- Load texture into texture list
 	local idx = ShroudLoadTexture(path, true)
 	if idx > 0 then
@@ -13,7 +13,7 @@ module.LoadTexturePixel = function(color, path) {
 	end
 end
 
-module.GetTextureId = function(color) {
+module.GetTextureId = function(color)
 	-- Get texture ID from texture name
 	if module.textures[color] then
 		return module.textures[color]
@@ -21,7 +21,7 @@ module.GetTextureId = function(color) {
 	return -1
 end
 
-module.PrepareSolidElement = function(handle, idx1, idx2, color) {
+module.PrepareSolidElement = function(handle, idx1, idx2, color)
 	-- Prepare a Solid Texture Element (create a new one if not exists)
 	local key = handle .. "-" .. idx1 .. "-" .. idx2
 	if module.ui_objects[handle] then
@@ -78,6 +78,61 @@ module.drawLine = function (X1, Y1, X2, Y2)
 	ShroudSetTransparency(module.current_ui_object, UI.Image, 1 - Y1 / ShroudGetScreenY())
 end
 
+module.DrawAngularPath = function(handle, pathCollection, color, radius_multiplier, angle_offset)
+	color             = color or "white"
+	radius_multiplier = radius_multiplier or 1
+	angle_offset      = angle_offset or 0
+	
+	local PX = ShroudPlayerX
+	local PY = ShroudPlayerY + 4
+	local PZ = ShroudPlayerZ
+	local PB = ShroudGetPlayerOrientation() + 0
+
+	-- Last values
+	local LA, LR, LX, LY, LZ, LSX, LSY = 0, 0, 0, 0, 0, 0, 0
+	-- new values
+	local NA, NR, NX, NY, NZ, NSX, NSY = 0, 0, 0, 0, 0, 0, 0
+
+	local flag_drawLine = 0	
+	-- adjust from UI size differences
+	radius_multiplier = radius_multiplier * 0.9
+	
+	for pathidx, pathArray in ipairs(pathCollection) do        
+		-- We want to skip first element
+		flag_drawLine = 0
+		for i = 1, #pathArray / 2 do
+			NA = pathArray[i * 2 -1]
+			NR = pathArray[i * 2]
+
+			--ShroudConsoleLog("Angle: " .. NA .. ", radius: " .. NR)
+
+			if NR < 0 then
+				-- If radius is negative, do not draw line
+				flag_drawLine = 0
+				NR = -NR
+			end
+
+			local _NA = math.rad(NA - PB + angle_offset)
+
+			-- calculate coordinates
+			NX = PX + math.sin(_NA) * NR * radius_multiplier
+			NY = PY
+			NZ = PZ - math.cos(_NA) * NR * radius_multiplier
+
+			vOut = ShroudWorldToScreenPoint(NX, NY, NZ)
+			NSX = vOut.x
+			NSY = vOut.y
+
+			if flag_drawLine != 0 then
+				module.PrepareSolidElement(handle, pathidx, i, color)
+				module.DrawLine(LSX, LSY, NSX, NSY)
+			end
+			LA = NA; LR = NR; LX = NX; LY = NY; LZ = NZ; LSX = NSX; LSY = NSY;
+			flag_drawLine = 1
+		end
+	end
+end
+	
 
 
 
