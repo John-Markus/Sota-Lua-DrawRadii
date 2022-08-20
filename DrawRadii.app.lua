c1e4aa2a087c28e3fb6c452d4cd24e7cb06ce310
John Markus <todam@local.io>
2022-08-20 11:11:10 +0900

    Use modules

--- DrawRadii.app.lua
+++ DrawRadii.app.lua
@@ -4,15 +4,11 @@ local CreatorName = "John Markus";
 local Description = "Draw skill circle around you";
 local IconPath = "Sota-Lua-DrawRadii/appicon.png";
 
-
+local UID = false;
+local animation_counter = 0
 
 config = {
-    is_ui_initialized = 0,
-    gui_draw_once = 0,
-    textures = {},
-    uiobjects = {},
-    current_ui_object = false,
-    was_ui_visible = 0,
+
 }
 
 shapes = {
@@ -27,10 +23,12 @@ localizations = {
 -- This is for loading additional lua modules.
 -- Use variable = init_<filename>() to obtain the object
 function importModule(filename)
-    local file = io.open("Sota-Lua-DrawRadii/modules/" .. filename .. ".lua")
+    local _path = ShroudLuaPath .. "/Sota-Lua-DrawRadii/modules/lib_" .. filename .. ".lua"
+    local file = io.open(_path)
     local data = file:read("*all")
     file:close()
     _G["init_" .. filename] = assert(loadsafe(data))
+    ShroudConsoleLog("Loaded lua module: " .. _path)
 end
 
 -- localization function
@@ -43,156 +41,14 @@ function __(ident, msg)
 end
 
 function LoadTexturePixel(color) 
-    local idx = ShroudLoadTexture("Sota-Lua-DrawRadii/images/" .. color .. ".png", true)
-    if idx > 0 then
-        config.textures[color] = idx
-        ShroudConsoleLog("texture loaded: " .. color .. "=" .. idx)
-    else
-        ShroudConsoleLog("texture failed: " .. color )
-    end
-end
-
-function InitApp()
-    ShroudConsoleLog('Lua Plugin: Draw Radii initialized.')
-
-
-end
-
-
-
-function ui_drawline(X1, Y1, X2, Y2, ui)
-    if config.current_ui_object == -1 then
-        return
-    end
-    
-    if X1 < 0 or Y1 < 0 or X2 < 0 or Y2 < 0 then 
-        ShroudHideObject(config.current_ui_object, UI.Image)
-        return
-    end
-    
-    if X1 > ShroudGetScreenX() or X2 > ShroudGetScreenX() then
-        ShroudHideObject(config.current_ui_object, UI.Image)
-        return
-    end
-    if Y1 > ShroudGetScreenY() or Y2 > ShroudGetScreenY() then
-        ShroudHideObject(config.current_ui_object, UI.Image)
-        return
-    end
-    --ShroudConsoleLog("DrawLine (" .. X1 .. "," .. Y1 .. ") - (" .. X2 .. ","  .. Y2 .. ")")
-
-    local _size = math.ceil(math.sqrt(math.pow(X2-X1,2) + math.pow(Y2-Y1,2)))
-    local _slope = math.deg(math.atan2(X2 - X1 , Y2 - Y1)) + 270
-    local _depth = math.max(1, 4 - math.ceil(Y1 / ShroudGetScreenY() * 3))
-
-    
-
-    ShroudSetPosition(config.current_ui_object, UI.Image, X1, Y1)
-    ShroudSetSize(config.current_ui_object, UI.Image, _size, _depth)
-    ShroudRotateObject(config.current_ui_object, UI.image, _slope)
-    ShroudShowObject(config.current_ui_object, UI.Image)
-    ShroudSetTransparency(config.current_ui_object, UI.Image, 1 - Y1 / ShroudGetScreenY())
-
-end
-
-function fit360(angle) 
-    while angle < 0 do
-        angle = angle + 360
-    end
-    while angle >= 360 do
-        angle = angle - 360
-    end
-    return angle
-end    
-
-function ui_hideUIObjects(handle) 
-    if config.uiobjecs[handle] then
-    end    
-end
-
-function ui_get_texture(color)
-    if config.textures[color] then
-        return config.textures[color]
-    end
-    ShroudConsoleLog("texture missing: " .. color)
-    return -1
-end
-
-function ui_prepare_ui_object(handle, idx1, idx2, color)
-    local key = handle .. '-' .. idx1 .. '-' .. idx2
-    if config.uiobjects[handle] then        
-        if config.uiobjects[handle][key] then
-            config.current_ui_object = config.uiobjects[handle][key]
-            return config.current_ui_object
-        end
-    else   
-        config.uiobjects[handle] = {}
-    end
-
-    local _texture = ui_get_texture(color)
-    config.current_ui_object = ShroudUIImage(0, 0, 1, 1, _texture)
-    ShroudSetTransparency(config.current_ui_object, UI.Image, 1)
-    ShroudHideObject(config.current_ui_object, UI.Image)
-    
-    
-    config.uiobjects[handle][key] = config.current_ui_object
-    return config.current_ui_object
-end
-
-
-function DrawAngularPath(handle, pathCollection, radius_multiplier, color) 
-    local PX = ShroudPlayerX
-    local PY = ShroudPlayerY + 4
-    local PZ = ShroudPlayerZ
-    local PB = fit360(ShroudGetPlayerOrientation())
-
-    local LA, LR, LX, LY, LZ, LSX, LSY = 0, 0, 0, 0, 0, 0, 0
-    local NA, NR, NX, NY, NZ, NSX, NSY = 0, 0, 0, 0, 0, 0, 0
-
-    local flag_drawLine = 0
-
-    radius_multiplier = radius_multiplier * 0.9
-
-    for pathidx, pathArray in ipairs(pathCollection) do        
-        flag_drawLine = 0
-        for i = 1, #pathArray / 2 do
-            NA = pathArray[i * 2 -1]
-            NR = pathArray[i * 2]
-
-            --ShroudConsoleLog("Angle: " .. NA .. ", radius: " .. NR)
-
-            if NR < 0 then
-                -- If radius is negative, do not draw line
-                flag_drawLine = 0
-                NR = -NR
-            end
-
-            local _NA = math.rad(NA - PB)
-
-            -- calculate coordinates
-            
-            NX = PX + math.sin(_NA) * NR * radius_multiplier
-            NY = PY
-            NZ = PZ - math.cos(_NA) * NR * radius_multiplier
-
-            vOut = ShroudWorldToScreenPoint(NX, NY, NZ)
-            NSX = vOut.x
-            NSY = vOut.y
-
-            if flag_drawLine != 0 then
-                local _ui = ui_prepare_ui_object(handle, pathidx, i, color)
-                ui_drawline(LSX, LSY, NSX, NSY, _ui)
-            end
-            LA = NA; LR = NR; LX = NX; LY = NY; LZ = NZ; LSX = NSX; LSY = NSY;
-            flag_drawLine = 1
-        end
-        
-
-    end
-
-
+    local _path = "Sota-Lua-DrawRadii/images/" .. color .. ".png"
+    UID.LoadTexturePixel(color, _path)
 end
 
 function ShroudOnStart()
+    importModule('draw_ui')
+    UID = init_draw_ui()
+
     -- Load additional modules using importModule() here
     LoadTexturePixel('white')
     LoadTexturePixel('black')
@@ -206,68 +62,88 @@ function ShroudOnStart()
     LoadTexturePixel('magenta')
 
     shapes.circle = {}
+    shapes.wavy_circle = {}
     for i = 0, 360, 10 do
-        table.insert(shapes.circle, i);
-        table.insert(shapes.circle, 1);
+        table.insert(shapes.circle, {i, 1, 0});
+        table.insert(shapes.wavy_circle, {i, 1, math.sin(math.rad(i * 10))/10});
     end
-
-    --for i = 1, ShroudGetStatCount() do
-    --    ShroudConsoleLog(i .. ': ' .. ShroudGetStatNameByNumber(i) .. ' = ' .. ShroudGetStatValueByNumber(i))
-    --end
-
-
 end
 
 function ShowVisualizations()
-    config.was_ui_visible = 10
-    DrawAngularPath('x', {{45,0.1,225,0.1},{135,0.1,315,0.1}}, 4.9, "white")
-    DrawAngularPath('Spear Radii', {shapes.circle}, 4.9, "yellow")
+    UID.UpdateAvatarLocation()
+    UID.ClearUsageTracking()
+
+    animation_counter = animation_counter + 30
+    if animation_counter >= 360 then
+        animation_counter = 0
+    end
+
+    -- bard skills has fixed range
+    local bard_skill_range = 5 * (1 + ShroudGetStatValueByName("ResoundingReachBonus"))
+    local BardSkills = {}
+    BardSkills["MelodyOfMending"] = "green"
+    BardSkills["AnthemOfAlacrity"] = "blue"
+    BardSkills["PsalmOfStagnation"] = "yellow"
+    BardSkills["AtonalAria"] = "red"
+    BardSkills["SavageSonata"] = "cyan"
+    BardSkills["ConcussiveCanticle"] = "red"
+    BardSkills["MesmerizingMelody"] = "yellow"
+    BardSkills["RhapsodyOfRecovery"] = "green"
+    BardSkills["RefrainOfResistance"] = "blue"
+
+    local _buffs = ShroudGetPlayerBuff()
     
-    DrawAngularPath('Spinning Radii', {shapes.circle}, 6.7, "cyan")
-    DrawAngularPath('Pull Radii', {shapes.circle}, 7.9, "white")
+    -- draw UI elements that are aligned to the character
+    UID.config.align_to_camera  = 0 
+    UID.DrawAngularPath('x', { {{180,1},{0,1},{90,1},{270,1},{0,1}} }, "white", 0.3, 0, 0.1)
+
+    local bard_skill_in_use = 0
+
+    for i, v in pairs(_buffs) do
+        if BardSkills[v.runeName] then
+            bard_skill_in_use = bard_skill_in_use + 1
+            UID.DrawAngularPath(v.runeName, {shapes.wavy_circle}, BardSkills[v.runeName], bard_skill_range, animation_counter + bard_skill_in_use * 15)
+        end
+    end
+    if bard_skill_in_use == 0 then
+        UID.DrawAngularPath("BardSkillCircle", {shapes.circle}, "white", bard_skill_range, 0, 0.1)
+    end
+
+    -- draw UI element that are aligned to the camera
+    UID.config.align_to_camera = 1
+
+    UID.HideUnused()
+end
+
+function DisplayBuffs()
+    local _buffs = ShroudGetPlayerBuff()
+    if _buffs then
+        for i,v in pairs(_buffs) do
+            ShroudConsoleLog(v.runeName);
+            for x,y in pairs(v.effects) do
+                ShroudConsoleLog(y.value .. " " .. y.description .. " Duration: " .. y.currentDuration .. "/" .. y.totalDuration .. " Ticks: " .. y.totalTick);
+            end           
+        end
+    end    
 
 end
 
 function HideVisualizations()
-    if config.was_ui_visible <= 0 then
-        return
-    end
-
-    config.was_ui_visible = config.was_ui_visible - 1
-    for handle, objectAr in pairs(config.uiobjects) do
-        for key, objectId in pairs(objectAr) do
-            if config.was_ui_visible > 0 then
-                ShroudSetTransparency(objectId,UI.Image, config.was_ui_visible / 10)
-            else
-                ShroudHideObject(objectId,UI.Image)
-            end
-        end
-    end    
-
-
+    UID.HideVisualizations()
 end
 
-
 function ShroudOnGUI()
-
-
 end
 
 function ShroudOnUpdate()
-    if ShroudGetPlayerCombatMode() then
-        ShowVisualizations()
-    else
-        HideVisualizations()
-
+    if UID then
+        if ShroudGetPlayerCombatMode() then
+            ShowVisualizations()
+        else
+            HideVisualizations()
+        end
     end
 end
 
 function ShroudOnConsoleInput(type, sourcePlayer, message)
 end
-
-
-
-
-
-
-
--- modules/lib_draw_ui.lua
+++ modules/lib_draw_ui.lua
@@ -1,9 +1,55 @@
 -- draw_ui module for Lua
 
 local module = { }
+module.config = {}
+module.config.ui_fade_timeout = 10
+module.config.show_verbose_msg = 1
+module.config.avatar_height = 0
+module.config.distance_multiplier = 0.9
+module.config.align_to_camera = 0
+
+module.avatar = {x = 0, y = 0, z = 0, orientation = 0, ui_bearing = 0, is_firstperson = 0}
+
 module.textures = { }
 module.ui_objects = { }
+module.tracking = { }
 module.current_ui_object = false;
+module.was_ui_visible = 0
+
+module.VerboseMsg = function (text)
+	if module.config.show_verbose_msg then
+		ShroudConsoleLog(text)
+	end
+end
+
+module.ClearUsageTracking = function() 
+	module.tracking = { }
+end
+
+module.HideUnused = function()
+	for handle, objectsAr in pairs(module.ui_objects) do
+		if module.tracking[handle] then
+		else
+			for key, objectId in pairs(objectsAr) do
+				ShroudHideObject(objectId, UI.Image)
+			end
+		end
+	end
+end
+
+module.UpdateAvatarLocation = function()
+	module.avatar.x = ShroudPlayerX
+	module.avatar.y = ShroudPlayerY + module.config.avatar_height
+	module.avatar.z = ShroudPlayerZ
+	-- Direction the avatar is facing
+	module.avatar.orientation = ShroudGetPlayerOrientation() + 0
+
+	-- Direction the camera is facing
+	local vCenter = ShroudWorldToScreenPoint(module.avatar.x, module.avatar.y, module.avatar.z)
+	local vNorth = ShroudWorldToScreenPoint(module.avatar.x, module.avatar.y, module.avatar.z + 1)
+	module.avatar.ui_bearing = - math.floor(math.deg(math.atan2(vNorth.x - vCenter.x, vNorth.y - vCenter.y))) 
+
+end
 
 --- Load specified path as a texture
 -- @param color Internal name that should be used to recall the texture
@@ -13,6 +59,9 @@ module.LoadTexturePixel = function(color, path)
 	local idx = ShroudLoadTexture(path, true)
 	if idx > 0 then
 		module.textures[color] = idx
+		module.VerboseMsg("Texture loaded: " .. color .. " from " .. path)		
+	else
+		module.VerboseMsg("Texture load failed: " .. path)
 	end
 end
 
@@ -26,39 +75,39 @@ module.GetTextureId = function(color)
 	return -1
 end
 
---- Generate and cache texture element
+--- Generate and cache textured element
 -- @param handle
 -- @param idx1
 -- @param idx2
 -- @param color
-
 module.PrepareSolidElement = function(handle, idx1, idx2, color)
 	idx1 = idx1 or 1
 	idx2 = idx2 or 1
-	
 
 	-- Prepare a Solid Texture Element (create a new one if not exists)
 	local key = handle .. "-" .. idx1 .. "-" .. idx2
 	if module.ui_objects[handle] then
 		if module.ui_objects[handle][key] then
 			module.current_ui_object = module.ui_objects[handle][key]
-			return module.ui_objects[handle][key]
+			return module.current_ui_object
 		end
 	else
 		module.ui_objects[handle] = {}
 	end
 	
-	local _texture = module.GetTextureid(color)
+	local _texture = module.GetTextureId(color)
 	module.current_ui_object = ShroudUIImage(0, 0, 1, 1, _texture)
 	
 	ShroudSetTransparency(module.current_ui_object, UI.Image, 1)
 	ShroudHideObject(module.current_ui_object, UI.Image)
+	ShroudRaycastObject(module.current_ui_object, UI.Image, false)
 
 	module.ui_objects[handle][key] = module.current_ui_object
 	return module.current_ui_object
 end
 
-module.drawLine = function (X1, Y1, X2, Y2)
+module.DrawLine = function (X1, Y1, X2, Y2, alpha)
+	alpha = alpha or 1
 	-- Draw a line using Solid Texture (Use Prepare Solid Element first)
 	if module.current_ui_object == -1 then
 		return
@@ -71,7 +120,7 @@ module.drawLine = function (X1, Y1, X2, Y2)
 	end
 
 	if X1 > ShroudGetScreenX() or X2 > ShroudGetScreenX() then
-		ShroudHideObject(module.module.current_ui_object, UI.Image)
+		ShroudHideObject(module.current_ui_object, UI.Image)
 		return
 	end
 	if Y1 > ShroudGetScreenY() or Y2 > ShroudGetScreenY() then
@@ -90,34 +139,62 @@ module.drawLine = function (X1, Y1, X2, Y2)
 	ShroudSetSize        (module.current_ui_object, UI.Image, _size, _depth)
 	ShroudRotateObject   (module.current_ui_object, UI.image, _slope)
 	ShroudShowObject     (module.current_ui_object, UI.Image)
-	ShroudSetTransparency(module.current_ui_object, UI.Image, 1 - Y1 / ShroudGetScreenY())
+	ShroudSetTransparency(module.current_ui_object, UI.Image, (1 - Y1 / ShroudGetScreenY()) * alpha)
+
+	module.was_ui_visible = module.config.ui_fade_timeout
 end
 
-module.DrawAngularPath = function(handle, pathCollection, color, radius_multiplier, angle_offset)
+module.ConvertAngleToScreen = function(angle, radius, yoffset)
+	yoffset = yoffset or 0
+
+	local _angle = math.rad(angle)
+
+	local x = module.avatar.x + math.sin(_angle) * radius
+	local y = module.avatar.y + yoffset
+	local z = module.avatar.z + math.cos(_angle) * radius
+
+	local vOut = ShroudWorldToScreenPoint(x, y, z)
+	vOut.y = ShroudGetScreenY() - vOut.y
+	return vOut
+end
+
+module.DrawAngularPath = function(handle, pathCollection, color, radius_multiplier, angle_offset, alpha)
 	color             = color or "white"
 	radius_multiplier = radius_multiplier or 1
 	angle_offset      = angle_offset or 0
-	
-	local PX = ShroudPlayerX
-	local PY = ShroudPlayerY + 4
-	local PZ = ShroudPlayerZ
-	local PB = ShroudGetPlayerOrientation() + 0
+	alpha             = alpha or 1
 
+	module.tracking[handle] = 1
+	
+	local PX = module.avatar.x
+	local PY = module.avatar.y
+	local PZ = module.avatar.z
+	local PB = 0
+	if module.config.align_to_camera == 0 then
+		PB = module.avatar.orientation 
+	else
+		PB = module.avatar.ui_bearing
+	end
+	
+	
 	-- Last values
 	local LA, LR, LX, LY, LZ, LSX, LSY = 0, 0, 0, 0, 0, 0, 0
 	-- new values
 	local NA, NR, NX, NY, NZ, NSX, NSY = 0, 0, 0, 0, 0, 0, 0
 
 	local flag_drawLine = 0	
+	local NOfs
 	-- adjust from UI size differences
-	radius_multiplier = radius_multiplier * 0.9
+	radius_multiplier = radius_multiplier * module.config.distance_multiplier
 	
 	for pathidx, pathArray in ipairs(pathCollection) do        
 		-- We want to skip first element
 		flag_drawLine = 0
-		for i = 1, #pathArray / 2 do
-			NA = pathArray[i * 2 -1]
-			NR = pathArray[i * 2]
+		for i = 1, #pathArray do
+			-- get angle and radius from pathArray
+			NA = pathArray[i][1]
+			NR = pathArray[i][2]
+			NOfs = pathArray[i][3] or 0
 
 			--ShroudConsoleLog("Angle: " .. NA .. ", radius: " .. NR)
 
@@ -127,20 +204,14 @@ module.DrawAngularPath = function(handle, pathCollection, color, radius_multipli
 				NR = -NR
 			end
 
-			local _NA = math.rad(NA - PB + angle_offset)
+			vOut = module.ConvertAngleToScreen(NA + PB + angle_offset, NR * radius_multiplier, NOfs)
 
-			-- calculate coordinates
-			NX = PX + math.sin(_NA) * NR * radius_multiplier
-			NY = PY
-			NZ = PZ - math.cos(_NA) * NR * radius_multiplier
-
-			vOut = ShroudWorldToScreenPoint(NX, NY, NZ)
 			NSX = vOut.x
 			NSY = vOut.y
 
 			if flag_drawLine != 0 then
 				module.PrepareSolidElement(handle, pathidx, i, color)
-				module.DrawLine(LSX, LSY, NSX, NSY)
+				module.DrawLine(LSX, LSY, NSX, NSY, alpha)
 			end
 			LA = NA; LR = NR; LX = NX; LY = NY; LZ = NZ; LSX = NSX; LSY = NSY;
 			flag_drawLine = 1
@@ -148,7 +219,22 @@ module.DrawAngularPath = function(handle, pathCollection, color, radius_multipli
 	end
 end
 	
+module.HideVisualizations = function()
+    if module.was_ui_visible <= 0 then
+        return
+    end
 
+    module.was_ui_visible = module.was_ui_visible - 1
+    for handle, objectAr in pairs(module.ui_objects) do
+        for key, objectId in pairs(objectAr) do
+            if module.was_ui_visible > 0 then
+                ShroudSetTransparency(objectId,UI.Image, module.was_ui_visible / module.config.ui_fade_timeout)
+            else
+                ShroudHideObject(objectId,UI.Image)
+            end
+        end
+    end    	
+end
 
 
 return module
